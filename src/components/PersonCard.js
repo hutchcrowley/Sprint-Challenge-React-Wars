76bc74c2b2fada3c396e13d5c104c041fd34dcca
Hutch Crowley <tothecoda@gmail.com>
2019-10-30 00:14:08 -0500

    updtate- rebuild

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,44 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container, Row } from 'reactstrap';
 
-PersonList = () =>
-{
-    const [people, setPeople] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get(`https://swapi.co/api/people/1/`)
-            .then(res => setPeople(res.data))
-            console.log(res.data)
-            .catch(err =>
-            {
-                'The API data was not returned', err;
-            }, []);
 
-        return(
-        <Container>
-            <Row
-                {people.map(person) => {
-                    <PersonCard
-                        name={person.name}
-                        height={person.height}
-                        weight={person.mass}
-                        hairColor={person.hair_color}
-                        birthday={person.birth_year}
-                        gender={person.gender}
-                        home={person.homeworld}
-                        species={person.species}
-                        films={person.films}
-                        vehicles={person.vehicles}
-                        ships={person.starships}
-                         />
-                })
-                }
+const PersonList = () => {
+  const [people, setPeople] = useState([])
 
-        </Container>
-    }
+
+   useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people')
+      .then(res => {
+
+        const personInfo = res.data;
+        setPeople(personInfo);
+        console.log('Star Wars API Person', personInfo);
+      })
+      .catch(err => {
+        console.log('The data was not returmed', err);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        {person.map((setPeople) => {
+          return (
+            name={person.name}
+            height={person.height}
+            weight={person.mass}
+            hairColor={person.hair_color}
+            birthday={person.birth_year}
+            gender={person.gender}
+            home={person.homeworld}
+            species={person.species}
+            films={person.films}
+            vehicles={person.vehicles}
+            ships={person.starships}
+          />
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
-
-
 export default PersonList
