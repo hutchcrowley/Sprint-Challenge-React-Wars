9a2a7757c7b0bbb18d63d28cfdfae4c0c365dcf9
Hutch Crowley <tothecoda@gmail.com>
2019-10-29 20:06:00 -0500

    update- stable build needed

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,49 +1,44 @@
-import React from 'react'
-import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Row } from 'reactstrap';
 
-
-const PersonCard = props =>
+PersonList = () =>
 {
-    return (
-        <Col xs='6' sm='4' lg='2' >
-            <Card className-''>
-                <CardBody>
-                <CardTitle>
-                    Character Name: {props.name}
-                </CardTitle>
-                <CardSubtitle>
-                    Species: {props.species}
-                </CardSubtitle>
-                <CardText>
-                    Gender: {props.gender}
-                </CardText>
-                <CardText>
-                    Height: {props.height}
-                </CardText>
-                <CardText>
-                    weight={props.weight}
-                </CardText>
-                <CardText>
-                    Hair Color: {props.hairColor}
-                </CardText>
-                <CardText>
-                    Homeworld: {props.home}
-                </CardText>
-                <CardText>
-                    Birth Year: {props.birthday}
-                </CardText>
-                <CardText>
-                    Films: {props.films}
-                </CardText>
-                <CardText>
-                    Vehicles: {props.vehicles}
-                </CardText>
-                <CardText>
-                    Ships: {props.ships}
-                </CardText>
-            </CardBody>
-            </Card>
-        </Col >
-    );
-};
-export default PersonCard
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people/1/`)
+            .then(res => setPeople(res.data))
+            console.log(res.data)
+            .catch(err =>
+            {
+                'The API data was not returned', err;
+            }, []);
+
+        return(
+        <Container>
+            <Row
+                {people.map(person) => {
+                    <PersonCard
+                        name={person.name}
+                        height={person.height}
+                        weight={person.mass}
+                        hairColor={person.hair_color}
+                        birthday={person.birth_year}
+                        gender={person.gender}
+                        home={person.homeworld}
+                        species={person.species}
+                        films={person.films}
+                        vehicles={person.vehicles}
+                        ships={person.starships}
+                         />
+                })
+                }
+
+        </Container>
+    }
+}
+
+
+export default PersonList
